50bc5fcc4aa7682e6013351671d6525f1f0895df
Daniel Bessone <ldb19852015@gmail.com>
2026-07-27 21:37:15 -0300

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,15 +1,19 @@
+// Dimensiones generales de la pantalla y físicas básicas
 export const SCREEN_WIDTH = 800;
 export const SCREEN_HEIGHT = 600;
 export const TILE_SIZE = 32;
 export const PLAYER_SPEED = 4;
-export const POOP_TIMER = 1000; // Milisegundos para explotar
+export const POOP_TIMER = 1000; // Tiempo en milisegundos antes de que explote la trampa
 
+// Tamaño de la grilla del mapa en bloques
 export const MAP_WIDTH_TILES = 25;
 export const MAP_HEIGHT_TILES = 18;
 
+// Cálculos para centrar perfectamente el mapa en el Canvas
 export const MAP_OFFSET_X = Math.floor((SCREEN_WIDTH - (MAP_WIDTH_TILES * TILE_SIZE)) / 2);
 export const MAP_OFFSET_Y = Math.floor((SCREEN_HEIGHT - (MAP_HEIGHT_TILES * TILE_SIZE)) / 2);
 
+// Matriz que define el diseño del nivel ('W' = Muro, 'D' = Destructible, '.' = Pasillo)
 export const LEVEL_LAYOUT = [
     "WWWWWWWWWWWWWWWWWWWWWWWWW",
     "W.......................W",
